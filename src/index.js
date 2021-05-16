f57db97c69ca544b9b28c7d96d9ab32adc65aa77
traja-traja <traja@traja.cz>
2021-05-16 10:58:41 +0200

    destrukturovani

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import "./style.css";
+
+// =========== UKOL DESTRUKTUROVÁNÍ //
+{
+
+    const product = {
+    name: "Toaster",
+    brand: "Tefal",
+    inStock: 34,
+    price: "$25",
+    colors: ["black", "red", "silver", "blue", "silver"],
+    };
+
+    /*
+        === ÚKOLY ===
+        1. Pomocí destrukturování získejte z objektu product jeho název a značku. Vypište je do konzole.
+        2. Pomocí destrukturování získejte z objektu product pole barev. Uložte si jej do proměnné productColors.
+        3. Z pole productColors si pomocí destrukturování vytáhněte první prvek do proměnné firstColor.
+        4. Z pole productColors si pomocí destrukturování vytáhněte do nějakých proměnných druhý a čtvrtý prvek.
+        5. Zkuste pomocí destrukturingu získat druhou a čtvrtou barvu přímo z objektu product.
+    */
+
+}
